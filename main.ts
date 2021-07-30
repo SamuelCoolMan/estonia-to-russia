e7ed98c60faeeaecd079d2d6763e60dc5e5a2360
SamuelCoolMan <88205217+SamuelCoolMan@users.noreply.github.com>
2021-07-30 10:14:17 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,11 @@
+controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+    scene.setBackgroundImage(assets.image`Restonia`)
+})
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+	
+})
+scene.setBackgroundImage(assets.image`Restonia`)
+let mySprite = sprites.create(assets.image`MAGIC`, SpriteKind.Player)
+forever(function () {
+	
+})
